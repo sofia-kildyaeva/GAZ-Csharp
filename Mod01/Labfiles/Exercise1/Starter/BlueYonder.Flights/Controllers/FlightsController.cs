80a4fe390a5f18f3dcc254982a0e807136a3bbb8
sofia-kildyaeva <81563074+sofia-kildyaeva@users.noreply.github.com>
2022-11-04 06:00:04 -0700

    Add files via upload

---
+++ Mod01/Labfiles/Exercise1/Starter/BlueYonder.Flights/Controllers/FlightsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlueYonder.Flights.Models;
+
+namespace BlueYonder.Flights.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        private readonly FlightsContext _context;
+
+        public FlightsController(FlightsContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/flights
+        [HttpGet]
+        public IEnumerable<Flight> Get()
+        {
+            return _context.Flights.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/flights
+        [HttpPost]
+        public IActionResult Post([FromBody]Flight flight)
+        {
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Get), flight.Id);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Mod01/Labfiles/Exercise1/Starter/BlueYonder.Flights/Models/FlightsContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueYonder.Flights.Models
+{
+    public class FlightsContext : DbContext
+    {
+        public FlightsContext(DbContextOptions<FlightsContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
